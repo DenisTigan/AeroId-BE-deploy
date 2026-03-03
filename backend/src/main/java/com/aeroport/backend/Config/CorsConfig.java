d0a4d5ad616dd1270d7e543a5d21d46b4a494f59
Lukas <lukas.patrikc@yahoo.com>
2026-03-03 16:05:52 +0200

    Cors added

---
+++ backend/src/main/java/com/aeroport/backend/Config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.aeroport.backend.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry config){
+        config.addMapping("/**")
+                .allowedOriginPatterns("https://localhost:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
--- backend/src/main/java/com/aeroport/backend/controller/EnrollController.java
+++ backend/src/main/java/com/aeroport/backend/controller/EnrollController.java
@@ -7,10 +7,7 @@ import org.springframework.http.HttpStatus;
 import com.aeroport.backend.service.CryptoService;
 import com.aeroport.backend.service.PythonIntegrationService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 
